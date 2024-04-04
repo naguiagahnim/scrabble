5ba5fda159b5d229142b5169f88f9548b03693ab
Gabriel GUY--SOULARD <gabrielguysoulard@gmail.com>
2024-04-04 10:29:59 +0200

    test premier commit proprio

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -4,5 +4,6 @@ public class ScrabbleApplicationConsole {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenue dans le scrabble");
+		System.out.println("Test premier commit");
 	}
 }
