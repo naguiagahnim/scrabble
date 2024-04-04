a914b598a632ee13646bad15722179979ce756c9
Gabriel GUY--SOULARD <gabrielguysoulard@gmail.com>
2024-04-04 10:53:35 +0200

    Création du fichier qui contiendra les jeux d'essai avec un Todo

---
+++ src/main/java/scrabble/application/ScrabbleJeuxEssais.java
@@ -0,0 +1,10 @@
+package scrabble.application;
+
+public class ScrabbleJeuxEssais {
+
+	public static void main(String[] args) {
+		// TODO Créer et gérer une pioche avec toutes les lettres + distribution initiale des lettres vers le chevalet + autre (à voir)
+
+	}
+
+}
