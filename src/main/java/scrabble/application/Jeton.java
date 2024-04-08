4d5cb87f8004a229805fab9bf43a3436afbef0a5
iut <benjamin.hollande@etu.unilim.fr>
2024-04-08 11:19:41 +0200

    commit enum jeton

---
+++ src/main/java/scrabble/application/Jeton.java
@@ -0,0 +1,31 @@
+package scrabble.application;
+
+public enum Jeton {
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+	I,
+	J,
+	K,
+	L,
+	M,
+	N,
+	O,
+	P,
+	Q,
+	R,
+	S,
+	T,
+	U,
+	V,
+	W,
+	X,
+	Y,
+	Z,
+	JOKER;
+}
