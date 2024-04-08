c571450ac4fc7db4715f06c9f9fd749effe39bc8
Victor Maguer <victor.maguer@etu.unilim.fr>
2024-04-08 11:46:39 +0200

    commit début du sac

---
+++ src/main/java/scrabble/model/sac.java
@@ -0,0 +1,20 @@
+package scrabble.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class sac {
+	private List<Jeton> jetonspioche = new ArrayList();
+
+	public List<Jeton> getJetonspioche() {
+		return jetonspioche;
+	}
+
+	public void setJetonspioche(List<Jeton> jetonspioche) {
+		this.jetonspioche = jetonspioche;
+	}
+	
+	public void melanger() {
+		
+	}
+}
