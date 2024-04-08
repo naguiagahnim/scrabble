959fd3688ad4c48e52ecc9fe1b78fc316ee7d7e8
Gabriel GUY--SOULARD <gabrielguysoulard@gmail.com>
2024-04-08 11:57:16 +0200

    modification de chevalet, erreur présente dans distribuer

--- src/main/java/scrabble/model/Chevalet.java
+++ src/main/java/scrabble/model/Chevalet.java
@@ -3,6 +3,7 @@ package scrabble.model;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Chevalet {
 	private List<Jeton> lettres = new ArrayList();
 	
@@ -11,6 +12,8 @@ public class Chevalet {
 	}
 	
 	public void distribuer() {
-		
+		for(Jeton jeton : Sac.jetonspioche) {
+			System.out.println("Jeton");
+		}
 	}
 }
