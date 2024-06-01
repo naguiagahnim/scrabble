70da42c3dff223deffd1697c302708645c5401d5
Agahnim <gabrielguysoulard@gmail.com>
2024-06-01 16:29:15 +0200

    Setup de l'IHM avec Maven, JavaFX et SceneBuilder

---
+++ src/main/java/scrabble/application/ScrabbleApplication.java
@@ -0,0 +1,27 @@
+package scrabble.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class ScrabbleApplication extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
+        Parent content = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ScrabbleView.fxml")));
+        root.setCenter(content);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Scrabble");
+        primaryStage.show();
+    }
+}
