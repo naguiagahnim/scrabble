f56b38b0495e8d0c02d06cce24ba8cc879c80874
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2024-06-02 15:21:40 +0200

    Bugfix pb de path Reconfiguration du pom.xml pour fonctionner avec la 1.8

--- src/main/java/scrabble/view/ScrabbleViewController.java
+++ src/main/java/scrabble/view/ScrabbleViewController.java
@@ -24,11 +24,11 @@ public class ScrabbleViewController {
     private void clicTheme(ActionEvent event) {
         Scene scene = ((Node) event.getSource()).getScene();
         if (isLightTheme) {
-            scene.getStylesheets().remove(Objects.requireNonNull(getClass().getResource("src/main/resources/stylesheet_light.css")).toExternalForm());
-            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/stylesheet_dark.css.css")).toExternalForm());
+            scene.getStylesheets().remove(Objects.requireNonNull(getClass().getResource("/stylesheet_light.css")).toExternalForm());
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/stylesheet_dark.css")).toExternalForm());
         } else {
-            scene.getStylesheets().remove(Objects.requireNonNull(getClass().getResource("src/main/resources/stylesheet_dark.css")).toExternalForm());
-            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("src/main/resources/stylesheet_light.css")).toExternalForm());
+            scene.getStylesheets().remove(Objects.requireNonNull(getClass().getResource("/stylesheet_dark.css")).toExternalForm());
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/stylesheet_light.css")).toExternalForm());
         }
         isLightTheme = !isLightTheme;
     }
