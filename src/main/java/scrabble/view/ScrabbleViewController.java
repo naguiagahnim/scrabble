023d76fd2c6061e248f40144c5384293225d6e06
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2024-06-03 11:01:24 +0200

    WIP IHM : plateau, chevalet et labels mis en place

--- src/main/java/scrabble/view/ScrabbleViewController.java
+++ src/main/java/scrabble/view/ScrabbleViewController.java
@@ -2,9 +2,12 @@ package scrabble.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,14 +16,51 @@ import java.util.Objects;
 public class ScrabbleViewController {
 
     @FXML
-    public GridPane grillePlateau;
+    private GridPane grillePlateau;
+
+    @FXML
+    private Label lettreChevalet1;
+
+    @FXML
+    private Label lettreChevalet2;
+
+    @FXML
+    private Label lettreChevalet3;
+
+    @FXML
+    private Label lettreChevalet4;
+
+    @FXML
+    private Label lettreChevalet5;
+
+    @FXML
+    private Label lettreChevalet6;
+
+    @FXML
+    private Label lettreChevalet7;
 
     @FXML
     private Button btnQuit;
 
     @FXML
     private void initialize() {
-        //initialisation
+        int numRows = 15;
+        int numCols = 15;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                Label label;
+                if (row == 7 && col == 7) {
+                    label = new Label("*");
+                } else {
+                    label = new Label(row + "_" + col);
+                }
+                label.setId("lbl" + row + "_" + col);
+                grillePlateau.add(label, col, row);
+                GridPane.setHalignment(label, HPos.CENTER);
+                GridPane.setValignment(label, VPos.CENTER);
+            }
+        }
     }
 
     @FXML
